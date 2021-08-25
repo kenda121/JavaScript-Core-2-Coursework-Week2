37338c9f105b8f08b5c7bee5a2424191d859fbf1
kenda <mariayousef2017@gmail.com>
2021-08-25 09:12:32 +0100

    not finish

--- projects/1-bike-project/js/main.js
+++ projects/1-bike-project/js/main.js
@@ -1,5 +1,11 @@
-function changecolor(blue){
-    let blue = document.querySelector("#blueBtn");
-    blue.addEventListener("click", changecolors);
-    function changecolors()
+let bluebutton = document.querySelector("#blueBtn");
+bluebutton.addEventListener("click", changecolor);
+function changecolor() {
+  let jomborton = document.querySelector(".jumbotron");
+  jomborton.style.background = "#588fbd";
+  let donate = document.querySelector(".buttons a");
+  donate.style.backgroundColor = "#ffa500";
+  let volunteer = document.querySelector(".buttons a:nth-child(2)");
+  volunteer.style.background = "black";
+  volunteer.style.color = "white";
 }
